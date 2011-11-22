41db02bc40480d53f31c85ffa93cac5d42e6955c
pierrchen <pierr.chen@gmail.com>
2011-11-22 00:44:13 -0800

    enable customized Theme to activity

---
+++ src/com/pierr/MySurfaceView.java
@@ -0,0 +1,135 @@
+package com.pierr;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+//import android.view.SurfaceHolder.Callback;
+
+public class MySurfaceView extends SurfaceView implements Runnable {
+
+	SurfaceHolder holder = null;
+	Thread t;
+	boolean shouldRun = false;
+
+	float x = 20;
+	float y = 20;
+
+	static final String TAG = "UnderstandAndroidUI";
+
+	public MySurfaceView(Context context) {
+
+		super(context);
+		// TODO Auto-generated constructor stub
+
+		holder = getHolder();
+
+		Log.d("UnderstandAndroidUI", "MySurfaceView is created");
+
+		t = new Thread(this);
+
+	}
+
+	public MySurfaceView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		Log.d("UnderstandAndroidUI", "MySurfaceView 2  is created");
+		// TODO Auto-generated constructor stub
+		init();
+	}
+
+	public MySurfaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		Log.d("UnderstandAndroidUI", "MySurfaceView 3 is created");
+		// TODO Auto-generated constructor stub
+
+		init();
+	}
+
+	public void init() {
+		holder = getHolder();
+
+		Log.d("UnderstandAndroidUI", "MySurfaceView is created");
+
+		t = new Thread(this);
+	}
+
+	static boolean isFirstTime = true;
+
+	void play() {
+
+		if (isFirstTime) {
+			isFirstTime = false;
+			shouldRun = true;
+			t.start();
+		} else {
+			resume();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+
+			// run it slowly , otherwise , pause/resume might be blocked as it won't be able 
+			// to get he monitor on this object by using synchronized
+			
+			//better to set the priority of this thread..
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException ex) {
+
+			}
+			synchronized (this) {
+				if (shouldRun == false) {
+					try {
+
+						Log.d("UnderstandAndroidUI", "Waiting to be run");
+						wait();
+					} catch (Exception ex) {
+
+					}
+				}
+
+				Canvas canvas = holder.lockCanvas();
+
+				int width = canvas.getWidth();
+				int height = canvas.getHeight();
+
+				Log.d("MySurfaceView", "width x height = " + width + ","
+						+ height);
+
+				y += 50;
+				Paint paint = new Paint();
+				paint.setARGB(255, 255, 0, 0);
+
+				canvas.drawCircle(x, y, 20, paint);
+
+				holder.unlockCanvasAndPost(canvas);
+			}
+		}
+	}
+
+	public void pause() {
+
+		Log.d(TAG, "enter on pause");
+		synchronized (this) {
+			shouldRun = false;
+
+		}
+	}
+
+	public void resume() {
+
+		synchronized (this) {
+			shouldRun = true;
+			notify();
+		}
+	}
+
+}
---
+++ src/com/pierr/TransparentActivity.java
@@ -0,0 +1,44 @@
+package com.pierr;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class TransparentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.transparent_layout);
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	
+	
+}
